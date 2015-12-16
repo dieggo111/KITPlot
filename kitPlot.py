5a9bf6871a3ece1f4ba92a11483bd7b993936fc1
Daniel Schell <daniel.schell@kit.edu>
2015-12-16 18:21:16 +0100

    small changes

--- kitPlot.py
+++ kitPlot.py
@@ -1,60 +1,70 @@
 import numpy as np
-from ROOT import *
+import ROOT
 
 class kitPlot():
 
-    def __init__(self, plot, x, y):
+    def __init__(self, plot=None, x=[], y=[]):
         
         self.__initStyle()
 
         if plot == "TGraph":
-            self.__graph = TGraph(len(x), np.asarray(x),np.asarray(y))
+            self.__graph = ROOT.TGraph(len(x), np.asarray(x),np.asarray(y))
+        else:
+            pass
+            
+
 
     def __initStyle(self):
 
         # Title Options
-        gStyle.SetTitleX(0.5)
-        gStyle.SetTitleY(0.97)
-        gStyle.SetTitleH(0.05)
+        ROOT.gStyle.SetTitleX(0.5)
+        ROOT.gStyle.SetTitleY(0.97)
+        ROOT.gStyle.SetTitleH(0.05)
         
         # Axis Options
-        gStyle.SetTitleSize(0.05,"X")
-        gStyle.SetTitleSize(0.05,"Y")
-        gStyle.SetTitleOffset(1.3,"X")
-        gStyle.SetTitleOffset(1.2,"Y")
+        ROOT.gStyle.SetTitleSize(0.05,"X")
+        ROOT.gStyle.SetTitleSize(0.05,"Y")
+        ROOT.gStyle.SetTitleOffset(1.3,"X")
+        ROOT.gStyle.SetTitleOffset(1.2,"Y")
         
-        gStyle.SetLabelSize(0.05,"X")
-        gStyle.SetLabelSize(0.05,"Y")
+        ROOT.gStyle.SetLabelSize(0.05,"X")
+        ROOT.gStyle.SetLabelSize(0.05,"Y")
         
         # Canvas Options
-        gStyle.SetPadBottomMargin(0.15)
-        gStyle.SetPadLeftMargin(0.15)
+        ROOT.gStyle.SetPadBottomMargin(0.15)
+        ROOT.gStyle.SetPadLeftMargin(0.15)
         
         # Legend Options
-        gStyle.SetLegendTextSize(0.035)
+        ROOT.gStyle.SetLegendTextSize(0.035)
         
         # Marker Options
-        gStyle.SetMarkerSize(1.5)
-        gStyle.SetMarkerStyle(3)
+        ROOT.gStyle.SetMarkerSize(1.5)
+        ROOT.gStyle.SetMarkerStyle(3)
 
         # Pad Options
-        gStyle.SetPadGridX(True)
-        gStyle.SetPadGridY(True)
+        ROOT.gStyle.SetPadGridX(True)
+        ROOT.gStyle.SetPadGridY(True)
+
+        return True
 
     def setAxisTitleSize(self, size):
 
-        gStyle.SetTitleSize(size,"X")
-        gStyle.SetTitleSize(size,"Y")
+        ROOT.gStyle.SetTitleSize(size,"X")
+        ROOT.gStyle.SetTitleSize(size,"Y")
         
         return True
 
     def setAxisTitleOffset(self, offset):
-        
-        gStyle.SetTitleOffset(offset,"X")
-        gStyle.SetTitleOffset(offset,"Y")
+
+        ROOT.gStyle.SetTitleOffset(offset,"X")
+        ROOT.gStyle.SetTitleOffset(offset,"Y")
+
+        return True
 
     def Draw(self, argument):
         self.__graph.Draw(argument)
+        
+        return True
 
     def getGraph():
         return self._graph
--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from kitPlot import kitPlot
 
 file1 = kitDataFile(sys.argv[1])
 
-kPlot = kitPlot("TGraph", file1.getX(), file1.getY())
+kPlot = kitPlot()
+g1 = TGraph(len(file1.getX()),file1.getX(1),file1.getY(1))
+g2 = TGraph(len(file1.getX()),file1.getX(1),file1.getZ(1))
+
 
 c1 = TCanvas("c1","c1",1280,768)
 c1.cd()
 
-kPlot.Draw("AP")
+c1.SetLogy()
+
+g2.Draw("AP")
+g1.Draw("SAME")
+
 c1.SaveAs("test.png")
     
