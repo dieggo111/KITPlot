f746f01466dd362381407f5b9df514297da8fd3c
dieggo111 <marius.metzler@student.kit.edu>
2017-02-03 14:46:00 +0100

    documentation v0.1 ready for rehearsal

--- KITPlot.py
+++ KITPlot.py
@@ -32,15 +32,77 @@ The script consists of 4 modules:
     d) The ConfigHandler module writes/reads/edits a plot-specific .cfg file. Since KITPlot is console-based and has no graphical interface, the config file solution
         makes up for it.
 
-2) Installation:
+3) Installation:
     a) Create a main folder and give it a nice name (f.e. 'KITPlot')
     b) Clone the KITPlot repository from 'https://github.com/SchellDa/KITPlot', put its content inside an extra folder within your main folder and name it 'modules'. If
         you feel the need to name it otherwise you will have to append its path the sys.path list). 
     c) Download and install python 2.7 on your system (https://www.python.org/downloads/).
-    d) The most recent version contains 'pip', a download manager/installer for python modules, which should be used to download the following modules: 'numpy', 
-        'mysql.connector, 'ConfigParser' and 'collections' (the rest should be standard python modules... there's nothing fancy here!').
+    d) The most recent version of python 2 contains 'pip', a download manager/installer for python modules, which should be used to download the following modules: 
+        'numpy', 'mysql.connector, 'ConfigParser' and 'collections' (the rest should be standard python modules... there's nothing fancy here!').
     e) Download and build ROOT v5.34/36 on your system. When building ROOT, make sure you enable the use of pyROOT. This is easy on Linux. However, doing this on Windows 
-        is a different story...
+        or Mac is a different story... although it's generally possible to do this on every system.
+    f) For security reasons the login
+
+4) Let's get started:
+    Before creating your first plot you need to write a few lines of code for yourself. Create a 'main.py' file import KITPlot and KITData as well as sys or other modules 
+    you need. KITPlot needs at least 1 arguement to do its magic. A second argument is optional. 
+        - First argument: data input. As described earlier, this can be a path of a file, folder or a run number from the database.
+        - Second argument: cfg file. If this is not given (None), then the script will search the cfg folder for a cfg file with the same name as the input. If you want to 
+            use a cfg file from another plot then this argument should be the path of this cfg file. Bottom line: names are important! Do not try to plot two folders that 
+            happen to have the same name. The former output will be overwritten with the new plot. 
+
+    A basic example of a main file could look like this:
+
+    ####################################################
+
+    import sys
+    import numpy as np
+    import ROOT 
+    sys.path.append('modules/')
+    from KITData import KITData
+    from KITPlot import KITPlot
+
+    if len(sys.argv) > 2:
+        kPlot1 = KITPlot(sys.argv[1],sys.argv[2])
+    else:
+        kPlot1 = KITPlot(sys.argv[1])
+
+
+    kPlot1.draw("APL")
+
+    raw_input()
+
+    ####################################################
+
+    
+    If no errors are being raised, the plot will show up on your screen. You can now start to edit plot with the related cfg file in your cfg folder.
+
+5) cfg file:
+    Most parameters in our cfg file are self-explanatory. Some have a special syntax that needs be considered or need some explanation:
+
+    - 'Range = [200:1000]': sets axis range from 200 to 1000 units. Mind the brackets!
+    - 'Font = 62': 62 is standard arial, bulky and ideal for presentations. See ROOT documention for other options.
+    - 'Log = False': This needs to be a boolean value. Remember that having a 0 in your data table may raise errors.
+    - 'Abs = True': This needs to be a boolean value.
+    - 'Title = Voltage (V)': You can announce special characters here with an '#' like '#circ', '#sigma' or super/subscript by '_{i}' and '^{2}'.
+    - 'GraphGroup = off': Default values are 'off', 'name', 'fluence'. Sometimes you might want to visualize that certain graphs belong together by giving them a similar 
+        color. 'off' will just alter marker color and style for every graph. By choosing 'name', all graphs that share the first 5 letters of their name will be drawn in 
+        the same color but with altering markers (f.e. sensors of the same type but from different wafers). If 'fluence' is choosen then then sensors with equal fluences 
+        will be drawn in the same color (the flunces are retreived from the database). Lastly, you can make your own 'GraphGroup' by using the original sensor order and 
+        put them into brackets like '[1,2][6][3,4,5]'.
+    - 'ColorShades = False': This needs to be a boolean value. If you use GraphGroups, you might as well want to use ColorShades. Let's say you have 3 red graphs and set 
+        this to True, then you will get 3 different kinds of red instead of one to make the graphs more distinctive and reconizable.
+    - 'Normalization = off': When ploting quantities like currents or resistances you might want to normalize them. This can be done by inserting the normalization factors 
+        (denominators) like '[7.590296,7.590296,1.277161,1.277161]' in respect of the original sensor order. In this case, every y-value of graph 1 and 2 (original sensor 
+        order) would be divided (normalized) by 7.590296.
+    - 'Position = auto': If this is set to auto then the scripts will search all 4 corners for the best spot to place the legend. You might wanna adjust this by using 'TR'
+        (top right corner), 'TL', 'BR' (bottom right corner) or 'BL'.
+    - 'BoxPara = 1': If you change the name of a legend element you might want to adjust the legend box width by changing this factor in between 0.5 and 1.5.
+    - 'SortPara = list': Pronounces the parameter that determines the order of all legend element.
+    - 'EntryList = (0)a, (1)b, (3)c, (2)d': The legend elements are naturally ordered by ROOT. This original order (here: a = 0, b = 1, c = 2, d = 3) can be edited by 
+        changing the number in the brackets. However, other options will always refere to the original order. 
+
+
 
 
 """
