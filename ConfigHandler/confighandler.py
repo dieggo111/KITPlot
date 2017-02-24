e8aa73ec2b9c740844b6103e15224a4c5ef90578
Daniel Schell <daniel.schell@kit.edu>
2017-02-24 14:53:11 +0100

    First implementation of the update method

--- ConfigHandler/confighandler.py
+++ ConfigHandler/confighandler.py
@@ -101,11 +101,11 @@ class ConfigHandler(object):
     def __getitem__(self,keys):
         try:
             return self.__cfg[keys]   
-        except(KeyError):
-            raise KeyError("Key not found")
+        except:
+            pass
 
         try:
-            return __getFormDict(self.__cfg,keys)
+            return self.__getFromDict(self.__cfg,keys)
         except:
             raise KeyError("Key not found")
 
@@ -120,15 +120,33 @@ class ConfigHandler(object):
             return os.path.splitext(os.path.basename(os.path.normpath(str(name))))[0] + ".cfg"
     
     # Get data from a dictionary with position provided as a list
-    def __getFromDict(self, dataDict, mapList):    
-        for k in mapList: dataDict = dataDict[k]
-        return dataDict
+    def __getFromDict(self, dataDict, mapList):
+        try:
+            for k in mapList: dataDict = dataDict[k]
+            return entry
+        except:
+            print("Raise exception")
+            raise Exception("Key not found")
     
     # Set data in a dictionary with position provided as a list
-    def __setInDict(self, dataDict, mapList, value): 
-        for k in mapList[:-1]: dataDict = dataDict[k]
-        dataDict[mapList[-1]] = value
+    def __setInDict(self, dataDict, mapList, value):
+        # Set new value if key already exists
+        try:
+            for key in mapList[:-1]: dataDict = dataDict[key]
+            dataDict[mapList[-1]] = value
+        except:
+            pass
 
+        # Set key in a multilevel dictionary
+        print("InputSubKey: " + str(mapList))
+        print("Subkeys: %s" %self.existingSubKeys(dataDict,mapList))
+        
+        
+#        try:
+#            for k in mapList[:-1]: dataDict = dataDict.setdefault(k,{})
+#            dataDict[mapList[-1]] = value
+#        except:
+#            raise Exception("Couldn't set parameter")
         
     # Old API
         
@@ -138,6 +156,84 @@ class ConfigHandler(object):
     def setParameter(self, cfg, sec, key, val):
         self.__cfg[sec][key] = val
         self.write(cfg)
+
+        
+    # Everything for the update function down here
+        
+    def update(self, comparison):
+
+        # Get the key lists
+        if isinstance(comparison,dict):
+            compDict = ConfigHandler()
+            compDict.setDict(comparison)
+        else:
+            compDict = ConfigHandler(comparison)
+
+        compKeys = compDict.keys()
+
+        print(self.keys())
+        print(compKeys)
+        
+        for key in compKeys:
+            if key not in self.keys():
+                self.__setInDict(self.__cfg,key,compDict[key])
+            else:
+                print("Present: %s" %key)
+                
+        print(self.keys())
+        print(compKeys)
+
+    def existingSubKeys(self, dataDict, keyList):
+        existingKeys = []
+        for key in keyList:
+            existingKeys.append(key)
+            try:
+                self.__getFromDict(dataDict,existingKeys)
+            except:
+                return existingKeys
+        
+    
+    def keys(self):
+        return(self.flatten(self.loop(self.__cfg)))
+        
+    def loop(self,dataDict):
+        keys = []
+        for key in dataDict:
+            if not isinstance(self.__getFromDict(dataDict,key),dict):
+                keys.append(key)            
+            else:
+                for addKey in self.loop(self.__getFromDict(dataDict,key)):
+                    keys.append([key,addKey])
+        return keys
+
+    def flatten(self,dataList):
+        """ Flatten multi level list down to two levels
+
+        """
+        
+        flattened = []
+        for item in dataList:
+            if not isinstance(item,list):
+                flattened.append(item)
+            else:
+                flattened.append(self.__flatten(item))
+
+        return flattened
+
+    def __flatten(self,dataList):
+        """ Completely flatten a multi level list
+
+        """
+        flattened = []
+        for item in dataList:
+            if not isinstance(item,list):
+                flattened.append(item)
+            else:
+                for subItem in self.__flatten(item):
+                    flattened.append(subItem)
+
+        return flattened
+
         
 if __name__ == '__main__':
 
@@ -146,12 +242,37 @@ if __name__ == '__main__':
                  "b": 2,
                  "c":
                  { "d": 3,
-                   "e": 4 }}
+                   "e": 4,
+                   "f": 5}}
 
+    print("Set first dictionary")
     cfg = ConfigHandler()
     cfg.setDict(testDict)
     cfg.write()
 
+    
+    print("Change one value")
     cfg["c","d"]=5
+
+    print("c,f: %s" %cfg["c", "f"])
+    
+    d = {'a': 1,
+         'b': 2,
+         'c': { 'd' : 3,
+                'e' : 4,
+                'f' : { 'f' : 5,
+                        'g' : 6
+                },
+                'h' : { 'i' : 7,
+                        'j' : { 'l' : 8
+                        }
+                },
+                'k' : 9
+         }
+    }
+
+    print("Update")
+    cfg.update(d)
+
     
                  
